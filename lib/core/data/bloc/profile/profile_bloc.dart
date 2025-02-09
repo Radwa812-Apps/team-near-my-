972f240c675ec5fed99e047796479dfc4612585a
roroemam <153381094+roroemam@users.noreply.github.com>
2025-02-09 12:20:12 +0200

    delete google accont done

--- lib/core/data/bloc/profile/profile_bloc.dart
+++ lib/core/data/bloc/profile/profile_bloc.dart
@@ -1,11 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
 
 import '../../models/user.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nearme_app/core/data/models/user.dart';
+import 'package:nearme_app/core/messages.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 part 'profile_event.dart';
 part 'profile_state.dart';
 
@@ -56,6 +65,41 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
     //   }
     // });
 
+    // on<DeleteUserEvent>((event, emit) async {
+    //   try {
+    //     emit(UserDeleteLoadingState());
+    //     User? user = FirebaseAuth.instance.currentUser;
+    //     if (user != null) {
+    //       final authCredential = EmailAuthProvider.credential(
+    //         email: event.email,
+    //         password: event.password,
+    //       );
+    //       await user.reauthenticateWithCredential(authCredential);
+    //       final customPlacesRef =
+    //           FirebaseFirestore.instance.collection('customPlaces');
+    //       final userCustomPlacesRef =
+    //           FirebaseFirestore.instance.collection('user_customPlaces');
+    //       final userCustomPlacesQuery =
+    //           userCustomPlacesRef.where('userId', isEqualTo: user.uid);
+    //       final userCustomPlacesSnapshot = await userCustomPlacesQuery.get();
+    //       for (var doc in userCustomPlacesSnapshot.docs) {
+    //         final customPlaceId = doc['customPlaceId'];
+    //         await customPlacesRef.doc(customPlaceId).delete();
+    //         await doc.reference.delete();
+    //       }
+    //       CollectionReference users =
+    //           FirebaseFirestore.instance.collection('users');
+    //       await users.doc(user.uid).delete();
+    //       await user.delete();
+    //       emit(UserDeletedSuccessState());
+    //     } else {
+    //       emit(UserDeleteErrorState(error: 'No user logged in'));
+    //     }
+    //   } catch (e) {
+    //     emit(UserDeleteErrorState(error: 'Failed to delete user: $e'));
+    //   }
+    // });
+
     on<DeleteUserEvent>((event, emit) async {
       try {
         emit(UserDeleteLoadingState());
@@ -63,11 +107,30 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         User? user = FirebaseAuth.instance.currentUser;
 
         if (user != null) {
-          final authCredential = EmailAuthProvider.credential(
-            email: event.email,
-            password: event.password,
-          );
-          await user.reauthenticateWithCredential(authCredential);
+          if (user.providerData
+              .any((info) => info.providerId == 'google.com')) {
+            final GoogleSignIn googleSignIn = GoogleSignIn();
+            final GoogleSignInAccount? googleAuth = await googleSignIn.signIn();
+
+            if (googleAuth != null) {
+              final GoogleSignInAuthentication googleAuthCredential =
+                  await googleAuth.authentication;
+
+              final AuthCredential credential = GoogleAuthProvider.credential(
+                accessToken: googleAuthCredential.accessToken,
+                idToken: googleAuthCredential.idToken,
+              );
+
+              await user.reauthenticateWithCredential(credential);
+            }
+          } else {
+            // إعادة المصادقة باستخدام EmailAuthProvider
+            final authCredential = EmailAuthProvider.credential(
+              email: event.email,
+              password: event.password,
+            );
+            await user.reauthenticateWithCredential(authCredential);
+          }
 
           final customPlacesRef =
               FirebaseFirestore.instance.collection('customPlaces');
